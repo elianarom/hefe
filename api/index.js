766b5737bb42b4d716d4380babce33bf25dc5adf
elianarom <168400508+elianarom@users.noreply.github.com>
2024-07-31 02:01:55 -0300

    conexion a la base de datos

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/api', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB: '));
+db.once('open', () => {
+    console.log('Conexión exitosa a MongoDB');
+});
 
 app.listen(3000, () => {
     console.log('server corriendo en el puerto 3000');
