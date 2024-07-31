17311d0a12c795259ab55a7f6c2074c9b9ac760a
elianarom <168400508+elianarom@users.noreply.github.com>
2024-07-31 01:29:16 -0300

    creacion del navbar

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -3,17 +3,21 @@ import Home from './vistas/Home'
 import Herramientas from './vistas/Herramientas'
 import PreguntasFrecuentes from './vistas/PreguntasFrecuentes'
 import IniciarSesion from './vistas/IniciarSesion'
+import Registrarse from './vistas/Registrarse'
 import CerrarSesion from './vistas/CerrarSesion'
 import Dashboard from './vistas/Dashboard'
+import Header from "./Componentes/Header"
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/herramientas" element={<Herramientas />} />
         <Route path="/preguntasfrecuentes" element={<PreguntasFrecuentes />} />
         <Route path="/iniciarsesion" element={<IniciarSesion />} />
+        <Route path="/registrarse" element={<Registrarse />} />
         <Route path="/cerrarsesion" element={<CerrarSesion />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
---
+++ cliente/src/Componentes/Header.jsx
@@ -0,0 +1,32 @@
+import { Button, Navbar } from "flowbite-react"
+import { FaMoon } from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
+import logo from '../assets/img/logo-hefe.svg'
+
+const Header = () => {
+  const path = useLocation().pathname;
+
+  return (
+    <Navbar>
+      <div className='flex flex-shrink-0 items-center'>
+        <img href="/" className="h-7 w-auto mr-8" src={logo} alt="logo hefe" />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={ path === "/" } as={'div'}><Link to="/">Home</Link></Navbar.Link>
+        <Navbar.Link active={ path === "/herramientas" } as={'div'}><Link to="/herramientas">Herramientas</Link></Navbar.Link>
+        <Navbar.Link active={ path === "/preguntasfrecuentes" } as={'div'}><Link to="/preguntasfrecuentes">Preguntas Frecuentes</Link></Navbar.Link>
+      </Navbar.Collapse>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-12 hidden sm:inline" color="blue-950" pill>
+          <FaMoon />
+        </Button>
+        <Link>
+          <Button className="py-1 px-6 text-lg rounded-full cursor-pointer font-medium bg-blue-700 text-white hover:bg-blue-950 shadow-xs transition-all duration-500">Iniciar Sesión</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+    </Navbar>
+  )
+}
+
+export default Header
