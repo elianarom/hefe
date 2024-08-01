8bc05ffe857d043d7562dc747ea478f3d0464e0a
elianarom <168400508+elianarom@users.noreply.github.com>
2024-07-31 22:49:37 -0300

    se agregó redux para el context

--- cliente/redux/store.js
+++ cliente/redux/store.js
@@ -1,8 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import usuarioReducer from './usuario/usuarioSlice'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers({
+    user: usuarioReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: usuarioReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false})
+});
+
+export const persistor = persistStore(store);
